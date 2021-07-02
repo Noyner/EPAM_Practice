709fdfc1f62e10ac711329cf16dc2a4660ef036c
Mark <basonmark4@gmail.com>
2021-07-03 00:01:44 +0400

    Three Layer

---
+++ Epam.UsersAwards.BLL.Interfaces/IAwardLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Epam.UsersAwards.Entities;
+using System.Collections.Generic;
+
+namespace Epam.UsersAwards.BLL.Interfaces
+{
+    public interface IAwardLogic
+    {
+        void AddAward(Award award);
+
+        IList<Award> AllAward();
+
+        void GiveAward(Guid userId, Guid awardId);
+    }
+}
---
+++ Epam.UsersAwardsDAL.Interfaces/IAwardDAO.cs
@@ -0,0 +1,15 @@
+﻿using Epam.UsersAwards.Entities;
+using System.Collections.Generic;
+using System;
+
+namespace Epam.UsersAwardsDAL.Interfaces
+{
+    public interface IAwardDAO
+    {
+        void AddAward(Award award);
+
+        IList<Award> AllAward();
+
+        void GiveAward(Guid userId, Guid awardId);
+    }
+}
